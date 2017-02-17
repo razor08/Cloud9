39e84150b05b602406b2740778583f0eb4260c10
Razor08 <razor.sinha.08@gmail.com>
2017-02-17 19:15:30 +0000

    Update before MongoDB integration

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,8 +3,8 @@ var app = express();
 var request = require("request");
 var bodyParser = require("body-parser");
 
-app.set(bodyParser.urlencoded({extended: true}));
-app.set(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 var campGrounds = [ 
@@ -19,14 +19,22 @@ app.get("/", function(req, res){
 });
 
 app.get("/campGrounds", function(req, res){
-   app.render("campGrounds", {CGList: campGrounds}); 
+   res.render("campGrounds", {CGList: campGrounds}); 
 });
 
-app.get("/campGrounds/new", function(req, res){
-    var newCG = req.query.new;
-    var imgSrc = req.query.img;
+app.post("/campGrounds/new", function(req, res){
+    var newCG = req.body.cgx;
+    var imgSrc = req.body.cimg;
+    console.log(newCG);
     var x = { name: newCG, img: imgSrc};
     campGrounds.push(x);
     res.redirect("/campGrounds");    
 });
 
+app.get("/*", function(req, res){
+    res.send("Sorry, page not found...What are you doing with your life ?");    
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Server is up and running! Go ahead make your move."); 
+});
